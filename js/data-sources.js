d4efc57a73417af05e947cacc559d7b2d04845ed
Baptiste BIEBER <baptiste.bieber@sowee.fr>
2019-11-25 11:35:12 +0100

    Correction jquery

--- js/data-sources.js
+++ js/data-sources.js
@@ -1,14 +1,14 @@
 
 function generate_filter() {
     var requestFilter = [];
-    jQuery('#filtersList tr[id^=filer_]').each(function() {
+    $('#filtersList tr[id^=filer_]').each(function() {
         var cond = "";
-        var ff = jQuery(this).find('select[id^=ff]').val();
-        var fo = jQuery(this).find('select[id^=fo]').val();
-        var fv = jQuery(this).find('*[id^=fv]').val();
-        var fv_date = jQuery(this).find('*[id^=fv][id$=date]').val();
-        var fv_date_b = jQuery(this).find('*[id^=fv][id$=date_b]').val();
-        var fieldType = jQuery(this).find('*[id^=fieldType]').val();
+        var ff = $(this).find('select[id^=ff]').val();
+        var fo = $(this).find('select[id^=fo]').val();
+        var fv = $(this).find('*[id^=fv]').val();
+        var fv_date = $(this).find('*[id^=fv][id$=date]').val();
+        var fv_date_b = $(this).find('*[id^=fv][id$=date_b]').val();
+        var fieldType = $(this).find('*[id^=fieldType]').val();
     //    console.log('ff: '+ff+' - '+'fo: '+fo+' - '+'fv: '+fv+' - '+'fv_date: '+fv_date+' - '+'fv_date_b: '+fv_date_b);
         if(ff != '') {
             cond = ff;
@@ -54,14 +54,14 @@ function generate_filter() {
         }
     });
     console.log(requestFilter.join(' and '));
-    jQuery('#filter_zoql').text(requestFilter.join(' and '));
+    $('#filter_zoql').text(requestFilter.join(' and '));
 }
 
 
 
 function page_execute() {
-    jQuery('#id_expand_Content li').each(function(){
-        var e = jQuery(this);
+    $('#id_expand_Content li').each(function(){
+        var e = $(this);
         var apiname = e.find('input').val();
         var fieldname = e.find('label').text().trim();
         if(apiname != null) {
@@ -73,38 +73,38 @@ function page_execute() {
             e.find('label').attr('for','input-'+apiname.replace(/\./g,'-'));
         }
     });
-    jQuery('#div_selected_ds').append(jQuery('<textarea id="select_zoql" rows="5" style="width:100%"></textarea>'));
-    jQuery('#div_selected_ds').append(jQuery('<button id="refresh_selected">Refresh</button>'));
-    jQuery('#refresh_selected').click(function(event) {
+    $('#div_selected_ds').append($('<textarea id="select_zoql" rows="5" style="width:100%"></textarea>'));
+    $('#div_selected_ds').append($('<button id="refresh_selected">Refresh</button>'));
+    $('#refresh_selected').click(function(event) {
         event.preventDefault();
-        var to_select = jQuery('#select_zoql').val().replace(/\s/g,'').split(',');
-        jQuery('#id_expand_Content li input:checked').each(function(){
-            jQuery(this).attr("checked",false);
+        var to_select = $('#select_zoql').val().replace(/\s/g,'').split(',');
+        $('#id_expand_Content li input:checked').each(function(){
+            $(this).attr("checked",false);
         });
-        jQuery.each(to_select, function(k,v) {
-            jQuery('#input-'+v.replace(/\./g,'-')).attr("checked",true);
+        $.each(to_select, function(k,v) {
+            $('#input-'+v.replace(/\./g,'-')).attr("checked",true);
         });
-        jQuery('#id_expand_Content table[id!=id_all_table]').each(function() {
-            updateCheckCount(jQuery(this).attr('id'));
+        $('#id_expand_Content table[id!=id_all_table]').each(function() {
+            updateCheckCount($(this).attr('id'));
         });
-        jQuery('#select_zoql').text(to_select.join(', '));
+        $('#select_zoql').text(to_select.join(', '));
     });
-    jQuery('#div_selected_ds').show();
+    $('#div_selected_ds').show();
     var selectedField = [];
-    jQuery('#id_expand_Content li input[type=checkbox]').change(function() {
+    $('#id_expand_Content li input[type=checkbox]').change(function() {
         selectedField = [];
-        jQuery('#id_expand_Content li input:checked').each(function(){
-            selectedField.push(jQuery(this).val());
+        $('#id_expand_Content li input:checked').each(function(){
+            selectedField.push($(this).val());
         });
-        jQuery('#select_zoql').val(selectedField.join(', '));
+        $('#select_zoql').val(selectedField.join(', '));
         return false;
     });
-    jQuery('#id_expand').append(jQuery('<a href="#" style="float:right" id="toggleapiname" alt="fieldname">Toggle API Name</a>'));
-    jQuery('#toggleapiname').click(function() {
-        var tmp = jQuery(this).attr('alt');
-        jQuery(this).attr('alt',(tmp == 'apiname' ? 'fieldname' : 'apiname'));
-        jQuery('#id_expand_Content li').each(function(){ 
-            var e = jQuery(this);
+    $('#id_expand').append($('<a href="#" style="float:right" id="toggleapiname" alt="fieldname">Toggle API Name</a>'));
+    $('#toggleapiname').click(function() {
+        var tmp = $(this).attr('alt');
+        $(this).attr('alt',(tmp == 'apiname' ? 'fieldname' : 'apiname'));
+        $('#id_expand_Content li').each(function(){ 
+            var e = $(this);
             var apiname = e.attr('apiname');
             var fieldname = e.attr('fieldname');
             if(tmp == 'apiname') {
@@ -118,15 +118,15 @@ function page_execute() {
         });
         return false;
     });
-    jQuery('#filtersList').append(jQuery('<textarea id="filter_zoql" rows="5" style="width:100%"></textarea>'));
-    jQuery('#filtersList').append(jQuery('<button id="generate_filter">generate</button>'));
-    jQuery('#generate_filter').click(function(event) {
+    $('#filtersList').append($('<textarea id="filter_zoql" rows="5" style="width:100%"></textarea>'));
+    $('#filtersList').append($('<button id="generate_filter">generate</button>'));
+    $('#generate_filter').click(function(event) {
         event.preventDefault();
         generate_filter();
     });
 };
 
 
-jQuery(document).ready(function() {
+$(document).ready(function() {
     page_execute();
 });
